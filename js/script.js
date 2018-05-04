2b28e2fb3f6c93f4994bdf02008b9cb4067bb898
romanvit13 <romanvit13@gmail.com>
2018-05-04 16:18:32 +0300

    version_4.4_release

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'AW-823828680');
+
+function gtag_report_conversion(url) {
+    var callback = function () {
+        if (typeof(url) != 'undefined') {
+            window.location = url;
+        }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-823828680/90DHCK6Uz30QyMHqiAM',
+        'transaction_id': '',
+        'event_callback': callback
+    });
+    return false;
+}
+
+jQuery(document).ready(function () {
+    jQuery(".eTimer").eTimer({
+        etType: 2,
+        etDate: "17.02.2018.0.0",
+        etTitleText: "",
+        etTitleSize: 20,
+        etShowSign: 1,
+        etSep: ":",
+        etFontFamily: "Trebuchet MS",
+        etTextColor: "#a3a3a3",
+        etPaddingTB: 15,
+        etPaddingLR: 15,
+        etBackground: "#333333",
+        etBorderSize: 0,
+        etBorderRadius: 2,
+        etBorderColor: "white",
+        etShadow: " 0px 0px 10px 0px #333333",
+        etLastUnit: 4,
+        etNumberFontFamily: "Impact",
+        etNumberSize: 35,
+        etNumberColor: "white",
+        etNumberPaddingTB: 0,
+        etNumberPaddingLR: 8,
+        etNumberBackground: "#636768",
+        etNumberBorderSize: 0,
+        etNumberBorderRadius: 5,
+        etNumberBorderColor: "white",
+        etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
+    });
+});
+
 $(document).ready(function(){
     $("#menu").on("click","a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
